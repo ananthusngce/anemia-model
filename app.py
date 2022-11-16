432b625348660bdf1f3427141e93019814b18fec
ananthusngce <111491482+ananthusngce@users.noreply.github.com>
2022-11-16 22:00:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+import joblib
+import traceback
+from flask_restful import reqparse
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "hey"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	lr = joblib.load("GDM_model.pkl")
+	if lr:
+		try:
+			json = request.get_json()	 
+			model_columns = joblib.load("model_cols.pkl")
+			temp=list(json[0].values())
+			vals=np.asarray(temp)
+			input_data_reshaped = vals.reshape(1,-1)
+			prediction = lr.predict(input_data_reshaped)
+			print("here:",prediction)        
+			return jsonify({'prediction': str(prediction[0])})
+
+		except:        
+			return jsonify({'trace': traceback.format_exc()})
+	else:
+		return ('No model here to use')
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
